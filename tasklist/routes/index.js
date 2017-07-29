43cd73ef37fbf339becac7a23edf56ed2abb4b12
Thomas M Maestas <tmilton.maestas@gmail.com>
2017-07-28 23:54:51 -0600

    mean started, no-connecte yet to mlab db

--- tasklist/routes/index.js
+++ tasklist/routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next){
-res.render('index.html'); //	res.send('TMJuliet INDEX PAGE');
+    res.render('index.html'); //	res.send('TMJuliet INDEX PAGE');
 });
 
 module.exports = router; 
--- tasklist/routes/tasks.js
+++ tasklist/routes/tasks.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
+// var mongojs = require('mongojs');
+// var db = mongojs('mongodb://thomasm1:Tmiltonm15%@ds147882.mlab.com:47882/tmjuillet', ['tasks']);
 
 router.get('/tasks', function(req, res, next){
-        res.send('TMTASK API');
-});
+//     db.tasks.find(function(err, tasks){
+//               if(err){
+  //                      res.send(err);
+     //            }
+    res.send('TMTASK API');  //     res.json(tasks);   //      //  res.send('TMTASK API'); 
+ });
+// });
 
 module.exports = router;
- 
