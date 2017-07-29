b82a2191d15ab42c9c0793e68b2d2510da4411c5
Thomas M Maestas <tmilton.maestas@gmail.com>
2017-07-29 00:55:54 -0600

    Changes to be committed: 	modified:   tasklist/package.json 	modified:   tasklist/routes/tasks.js 	modified:   tasklist/views/index.html

--- tasklist/routes/tasks.js
+++ tasklist/routes/tasks.js
@@ -8,8 +8,74 @@ router.get('/tasks', function(req, res, next){
 //               if(err){
   //                      res.send(err);
      //            }
-    res.send('TMTASK API');  //     res.json(tasks);   //      //  res.send('TMTASK API'); 
+    res.send('TM TASK API');  //     res.json(tasks);   //      //  
  });
 // });
 
-module.exports = router;
+// Get Single Task
+// router.get('/task/:id', function(req, res, next){
+  //   db.tasks.findOne({_id: mongojs.ObjectId(req.params.id)}, function(err, task){
+  //       if(err){
+ //            res.send(err);
+ //        }
+ //        res.json(task);
+ //    });
+// });
+
+//Save Task
+// router.post('/task', function(req, res, next){
+  //   var task = req.body;
+  //   if(!task.title || !(task.isDone + '')){
+  //       res.status(400);
+  //       res.json({
+  //           "error": "Bad Data"
+  //       });
+  //   } else {
+  //       db.tasks.save(task, function(err, task){
+  //           if(err){
+  //               res.send(err);
+   //          }
+    //         res.json(task);
+    //     });
+  //   }
+//  });
+
+// Delete Task
+// router.delete('/task/:id', function(req, res, next){
+//     db.tasks.remove({_id: mongojs.ObjectId(req.params.id)}, function(err, task){
+ //        if(err){
+ //            res.send(err);
+   //      }
+  //       res.json(task);
+ //    });
+// });
+
+// Update Task
+// router.put('/task/:id', function(req, res, next){
+  //   var task = req.body;
+  //   var updTask = {};
+  //   
+ //    if(task.isDone){
+ //        updTask.isDone = task.isDone;
+ //    }
+    
+ //    if(task.title){
+ //        updTask.title = task.title;
+ //    }
+    
+  //   if(!updTask){
+   //      res.status(400);
+   //      res.json({
+  //           "error":"Bad Data"
+  //       });
+  //   } else {
+    //     db.tasks.update({_id: mongojs.ObjectId(req.params.id)},updTask, {}, function(err, task){
+ ////         if(err){
+    //         res.send(err);
+    //     }
+    //     res.json(task);
+ //    });
+ //    }
+//  });
+
+module.exports = router; 
