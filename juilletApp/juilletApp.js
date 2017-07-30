d494f4c560b682b2a27272f4967c989d2d334a29
Thomas M Maestas <tmilton.maestas@gmail.com>
2017-07-30 15:27:03 -0600

    Express app final round

--- juilletApp/juilletApp.js
+++ juilletApp/juilletApp.js
@@ -1,19 +1,98 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
+var expressValidator = require('express-validator');
+
 var app = express();
 /* var logger = function(req, res, next){
     console.log('Tom's logging...');
     next();
 }
 app.use(logger);
-*/
+*/ 
+// View Engine
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+// body parser middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
+// set static path
+app.use(express.static(path.join(__dirname, 'public')));
+// Express Validator Middleware ::  the formParam value is going to get morphed into form body format useful for printing.
+app.use(expressValidator({
+  errorFormatter: function(param, msg, value) {
+      var namespace = param.split('.')
+      , root    = namespace.shift()
+      , formParam = root;
+
+    while(namespace.length) {
+      formParam += '[' + namespace.shift() + ']';
+    }
+    return {
+      param : formParam,
+      msg   : msg,
+      value : value
+    };
+  }
+}));
+    var person = [{
+        name: 'TMM',    age: 40, id: 1},
+        {name: 'tmm',    age: 3, id: 2},
+        {name: 'thomas',    age:88, id: 3}];
+
+    var users = [
+        { 
+            id: 1, 
+            fname: 'tom',
+            lname: 'milt',
+            email: 'thomas.maestas1@hotmail.com',
+        },  { 
+            id: 2, 
+            fname: 'tomtom',
+            lname: 'miltmilt',
+            email: 'thomas.maestas2@hotmail.com',
+        },  { 
+            id: 3, 
+            fname: 'tomtomtom',
+            lname: 'miltmiltmilt',
+            email: 'thomas.maestas3@hotmail.com',
+        }];
+  var titlejuillet = 'Juillet 1.0.0.3 FROM EJS';
 
 app.get('/', function(req, res){
-    res.send('Hello from Tom!');
+   // res.json(person); //   
+   // res.send('Hello from Tom. Express currently broadcasting on port 3000 !');
+    // res.render('index'); 
+    res.render('index', {
+        title: titlejuillet, 
+        users: users,
+        
+    });
+});
+app.post('/users/add', function(req, res){ 
+   req.checkBody('fname', 'First Name is Required').notEmpty();
+   req.checkBody('lname', 'Last Name is Required').notEmpty();
+   req.checkBody('email', 'Email is Required').notEmpty();
+
+    var errors = req.getValidationResult();
+    if(errors){
+        console.log('ERRORS');
+    } else {
+        var newUser = {
+            fname: req.body.fname, 
+            lname: req.body.lname,
+            email: req.body.email
+        }
+        console.log('SUCCESS')
+          }
+    
+    var newUser = {
+    fname: req.body.fname,
+    lname: req.body.lname, 
+    email: req.body.email
+}
+console.log(newUser);
 });
 app.listen(3000, function(){
-    console.log('Server started on Port 3000...');
+    console.log('Server starting, cough cough, on Port 3000...');
 })
