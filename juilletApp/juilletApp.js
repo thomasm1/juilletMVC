13a490867b397acd51b9a6b64dc2db78b2253966
Thomas M Maestas <tmilton.maestas@gmail.com>
2017-07-31 19:10:46 -0600

    prep-work to install angular directives

--- juilletApp/juilletApp.js
+++ juilletApp/juilletApp.js
@@ -2,7 +2,8 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
 var expressValidator = require('express-validator');
-
+// var mongojs = require('mongojs')
+// var db = mongojs(connectionString, [collections])
 var app = express();
 /* var logger = function(req, res, next){
     console.log('Tom's logging...');
@@ -15,9 +16,16 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 // body parser middleware
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({extended: false})); 
+
 // set static path
 app.use(express.static(path.join(__dirname, 'public')));
+
+// Global vars
+app.use(function(req, res, next){
+    res.locals.errors = null;
+    next();
+    });
 // Express Validator Middleware ::  the formParam value is going to get morphed into form body format useful for printing.
 app.use(expressValidator({
   errorFormatter: function(param, msg, value) {
@@ -35,6 +43,7 @@ app.use(expressValidator({
     };
   }
 }));
+
     var person = [{
         name: 'TMM',    age: 40, id: 1},
         {name: 'tmm',    age: 3, id: 2},
@@ -57,7 +66,7 @@ app.use(expressValidator({
             lname: 'miltmiltmilt',
             email: 'thomas.maestas3@hotmail.com',
         }];
-  var titlejuillet = 'Juillet 1.0.0.3 FROM EJS';
+var titlejuillet = 'Juillet 1.0.0.3 FROM EJS';
 
 app.get('/', function(req, res){
    // res.json(person); //   
@@ -66,33 +75,36 @@ app.get('/', function(req, res){
     res.render('index', {
         title: titlejuillet, 
         users: users,
-        
     });
 });
+
 app.post('/users/add', function(req, res){ 
+
    req.checkBody('fname', 'First Name is Required').notEmpty();
    req.checkBody('lname', 'Last Name is Required').notEmpty();
    req.checkBody('email', 'Email is Required').notEmpty();
-
-    var errors = req.getValidationResult();
+console.log('checking 1-2-3')
+console.log(req.checkBody.fname);
+    var errors = req.validationErrors(); // . Use req.getValidationResult() gives ERROR
+    
     if(errors){
-        console.log('ERRORS');
-    } else {
+        // res.json(person); //   
+   // res.send('Hello from Tom. Express currently broadcasting on port 3000 !'); 
+     res.render('index', {
+       title: titlejuillet, 
+       users: users,
+      errors: errors
+     });
+    } else { 
         var newUser = {
             fname: req.body.fname, 
             lname: req.body.lname,
             email: req.body.email
         }
-        console.log('SUCCESS')
-          }
-    
-    var newUser = {
-    fname: req.body.fname,
-    lname: req.body.lname, 
-    email: req.body.email
-}
-console.log(newUser);
+        console.log('SUCCESS');
+     }
 });
+
 app.listen(3000, function(){
     console.log('Server starting, cough cough, on Port 3000...');
 })
